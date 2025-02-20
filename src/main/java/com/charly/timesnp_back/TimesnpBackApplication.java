9eaf081f4a6a8458bb759458db236a63f1d7666c
CarlosHernandez14 <carloskvpchc@gmail.com>
2025-02-20 16:56:18 -0600

    First commit, set spring boot starters

---
+++ src/main/java/com/charly/timesnp_back/TimesnpBackApplication.java
@@ -0,0 +1,13 @@
+package com.charly.timesnp_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesnpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimesnpBackApplication.class, args);
+    }
+
+}
